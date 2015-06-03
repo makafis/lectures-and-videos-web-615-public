cddc1b8dddb74ab05f2b1d9af96ae835a12ab711
aviflombaum <avi@flombaum.com>
2015-06-03 09:32:16 -0400

    lecture 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# Videos
+
+[01 Variables and Methods]()
---
+++ ruby-lectures-1/my_first_program.rb
@@ -0,0 +1,8 @@
+def say_hello(phrase = "Hello World", the_number_of_times = 10)  
+  the_number_of_times.times do 
+    puts phrase
+  end
+end
+
+say_hello "Goodbye"
+
